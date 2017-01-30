2a0bba21c38536a82e7d9ff4ea054bcf5c7a61f9
Rohan Trivedi <rohan@wireless-10-147-160-26.public.utexas.edu>
2017-01-30 15:32:05 -0600

    added files

---
+++ MyFlicks/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  MyFlicks
+//
+//  Created by Rohan Trivedi on 1/30/17.
+//  Copyright © 2017 Rohan Trivedi. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
